3530e10b134ff312a5498c37032dd48c0fbe43d1
Blake Knight <bknight@gsandf.com>
2018-10-24 10:56:48 -0500

    :sparkles: add distance sensor

---
+++ packages/fortune-teller/distance-trigger.js
@@ -0,0 +1,39 @@
+const usonic = require('mmm-usonic-fixed');
+
+let sensor;
+
+function initSensor() {
+  return new Promise((resolve, reject) => {
+    if (sensor) return resolve(sensor);
+
+    usonic.init(err => {
+      if (err) return reject(err);
+      sensor = usonic.createSensor(24, 23, 450);
+      resolve(sensor);
+    });
+  });
+}
+
+async function watchForDistance(minDistance, debounceCount, cb) {
+  const sensor = await initSensor();
+  let timesSensed = 0;
+
+  return setInterval(() => {
+    // Convert distance to mm
+    const distance = sensor() * 10;
+
+    if (distance > minDistance) {
+      timesSensed = 0;
+      return;
+    }
+
+    if (++timesSensed > debounceCount) {
+      timesSensed = 0;
+      cb();
+    }
+  }, 1000);
+}
+
+module.exports = {
+  watchForDistance
+};
--- packages/fortune-teller/index.js
+++ packages/fortune-teller/index.js
@@ -1,4 +1,9 @@
 const { server } = require('./server');
+const { watchForDistance } = require('./distance-trigger');
+
+watchForDistance(1000, 3, () => {
+  console.log('Close enough');
+});
 
 server.start({}, () =>
   console.log('Server is running on http://localhost:4000')
