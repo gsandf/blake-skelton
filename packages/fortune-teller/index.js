9df6af3f5cbcea72e05a4b3862fa492da130a8df
Blake Knight <bknight@gsandf.com>
2018-10-24 13:59:00 -0500

    :sparkles: serve speech recognition page from same server as fortune-teller

--- packages/fortune-teller/index.js
+++ packages/fortune-teller/index.js
@@ -1,5 +1,17 @@
+const path = require('path');
+const express = require('express');
 const { server } = require('./server');
 
-server.start({}, () =>
-  console.log('Server is running on http://localhost:4000')
+server.express.use(
+  '/',
+  express.static(path.join(__dirname, '../speech-recognition/build'))
+);
+
+server.start(
+  {
+    endpoint: '/graphql',
+    playground: '/graphql',
+    subscriptions: '/graphql'
+  },
+  () => console.log('Server is running on http://localhost:4000')
 );
