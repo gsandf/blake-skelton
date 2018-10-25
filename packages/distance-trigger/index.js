871ccc41f21f86c73d80239230db1c58ebf55c32
Justyn Hunter <jhunter@gsandf.com>
2018-10-25 12:50:34 -0500

    added todos

--- packages/distance-trigger/index.js
+++ packages/distance-trigger/index.js
@@ -5,9 +5,11 @@ watchForDistance(1000, 1, (detected) => {
   if (detected) {
     console.log('Close enough');
     specialEffects(1, 1);
+    // todo initiate speech recognition    
   } 
   else {
     console.log('no one there');
     specialEffects(0, 0);
+    // todo turn off speech recognition
   }
 });
