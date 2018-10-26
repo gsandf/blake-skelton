b1df5b72b832510dae5f26f5184c71048a2dc829
Blake Knight <bknight@gsandf.com>
2018-10-25 23:32:10 -0500

    :bug: use right path for GraphQL

--- packages/fortune-teller/sync-effects.js
+++ packages/fortune-teller/sync-effects.js
@@ -1,6 +1,6 @@
 const createQuery = require('@blake-skelton/node-query');
 
-const query = createQuery('http://pieffects.local:4000');
+const query = createQuery('http://pieffects.local:4000/graphql');
 
 const setStateQuery = /* GraphQL */ `
   mutation($state: ApproachState!) {
